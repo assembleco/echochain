27a9dae02046bdd26908e215d72c6fdbce6b7406
grace <grace.c.youngblood@gmail.com>
2021-04-16 21:22:44 -0400

    Add a basic sidebar.

--- src/homescreen.js
+++ src/homescreen.js
@@ -3,7 +3,12 @@ import styled from "styled-components"
 
 const Homescreen = props => (
   <Screen>
-  Echochain
+    <Sidebar>
+      P<br/>
+      P<br/>
+      P<br/>
+      P<br/>
+    </Sidebar>
   </Screen>
 )
 
@@ -14,6 +19,15 @@ bottom: 0;
 left: 0;
 right: 0;
 top: 0;
+
+display: grid;
+grid-template-columns: 48px auto 1fr;
+`
+
+const Sidebar = styled.div`
+border: 4px solid #3d3b11;
+border-radius: 4px;
+padding: 12px;
 `
 
 export default Homescreen
