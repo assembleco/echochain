e86ecaa8104de3d3fd99b3b21a19f764b3035c87
grace <grace.c.youngblood@gmail.com>
2021-08-12 18:18:44 -0400

    Expand upon specifications.

--- spec/requests/signals_spec.rb
+++ spec/requests/signals_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Signals", type: :request do
   describe "GET /index" do
-    it "honors the 'hidden' label"
+    it "honors the 'hidden' code"
+    it "detects slash codes in the middle of a signal"
+    it "has no effect on the 'help' code"
   end
 end
