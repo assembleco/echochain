91d9c63c77a4b2b53f906c1a9cec2a14394695d5
grace <grace.c.youngblood@gmail.com>
2021-08-12 18:29:30 -0400

    Begin honoring hidden signals.

--- app/controllers/signals_controller.rb
+++ app/controllers/signals_controller.rb
@@ -1,5 +1,6 @@
 class SignalsController < ApplicationController
   def index
+    raise 'ohno! hidden signals are escaping!'
     render json: { signals: Sign.all }
   end
 end
--- spec/requests/signals_spec.rb
+++ spec/requests/signals_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Signals", type: :request do
   describe "GET /index" do
-    it "honors the 'hidden' code"
+    pending "honors the 'hidden' code" do
+      get "/signals", params: { hidden: "no" }
+    end
+
     it "detects slash codes in the middle of a signal"
     it "has no effect on the 'help' code"
   end
