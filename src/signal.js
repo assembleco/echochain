3b38d8a89a2d8bbc6e7083660c0fea5104705e14
grace <grace.c.youngblood@gmail.com>
2021-08-20 12:48:32 -0400

    Hook up a `<textarea/>` for editing signals.

--- src/signal.js
+++ src/signal.js
@@ -1,13 +1,36 @@
 import React from 'react'
 import styled from "styled-components"
 import ReactMarkdown from "react-markdown"
+import { Icon } from '@iconify/react';
 
 class Signal extends React.Component {
+  state = {
+    change: null,
+  }
+
   render = () => (
     <Border>
-      <ReactMarkdown>
-        {this.props.children}
-      </ReactMarkdown>
+      <Actions>
+        <Icon
+          icon="ci:edit"
+          onClick={() => {
+            this.setState({ change: this.props.children })
+          }}
+        />
+      </Actions>
+
+      {this.state.change
+        ?
+        <textarea
+        style={{ height: "20rem", width: "100%" }}
+        value={this.state.change}
+        />
+
+        :
+        <ReactMarkdown>
+          {this.props.children}
+        </ReactMarkdown>
+      }
     </Border>
   )
 }
@@ -17,6 +40,13 @@ border: 1px solid #3d3b11;
 border-radius: 4px;
 padding: 12px;
 background-color: #FAF9DD;
+position: relative;
+`
+
+var Actions = styled.div`
+position: absolute;
+top: 0;
+right: 0;
 `
 
 export default Signal
